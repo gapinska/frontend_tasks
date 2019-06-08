9d9c02adcd7658090ec86edffbfdaef6578392ea
gapinska <gapinska.mon@gmail.com>
2019-06-08 06:01:08 +0200

    variables task

---
+++ JavaScript_tasks/1_variables.js
@@ -0,0 +1,66 @@
+// TODO: 1. Podepnij ten plik do pliku index.html
+
+// TODO: 2. Zadeklaruj 2 zmienne typu string
+
+// TODO: 3. Zadeklaruj 2 zmienne typu number
+
+// TODO: 4. Zadeklaruj 2 zmienne typu boolean
+
+// TODO: 5. Zadeklaruj zmienna typu undefined
+
+// TODO: 6. Zadeklaruj zmienna typu null
+
+// TODO: 7. Zadeklaruj tablicę zawierajacą zmienne typu string
+
+// TODO: 8. Zadeklaruj tablicę zawierajacą zmienne różnego typu
+
+// TODO: 9. Zadeklaruj obiekt o nazwie person zawierajacy pola:
+//  - firstName
+//  - lastName
+//  - age
+//  - country
+
+// TODO: 10. Sprawdź typ co najmniej jeden zmiennej z poprzednich zadań
+
+// TODO: 11. Nadpisz wartość dowolnej zmiennej zadeklarownej w poprzednim zadaniu
+
+// TODO: 12. wyswietl w konsoli dane obiektu person w sensownym zdaniu typu:
+// Hej, jestem firstName lastName, mam age i jestem z country.
+
+// TODO: 13. nadpisz za pomocą dynamicznego klucza wartość country w obiekcie person
+// na inny kraj, wyswietl w konsoli tekst w taki sam sposob jak zadanie wyzej.
+var a = 'zadanie';
+
+var b = 'zrobione';
+
+var n = 1;
+var numb_1 = 2;
+
+var c = true;
+var d = false;
+
+var e = undefined;
+
+var f = null;
+
+var arr = ['one', 'two', 'three'],
+
+var arrMix = ['one', 2, true];
+
+var person= {
+    firstName: 'Monika',
+    lastName: 'Gapińska',
+    age: 30,
+    country: 'Poland'
+
+}
+
+typeof e;
+
+number = 5;
+
+console.log('Hej, jestem ' + person.firstName +' '+ person.lastName + ', ' +  'mam ' + person.age + ' lat i jestem z ' + person.country + '.')
+
+person.country = 'England';
+
+console.log('Hej, jestem ' + person.firstName +' '+ person.lastName +', '+ 'mam '+person.age+' lat i jestem z '+person.country+'.')
