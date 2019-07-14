96e4d55f41a79b5557da2d2d1777d0d3c99f8506
gapinska <gapinska.mon@gmail.com>
2019-07-14 19:40:45 +0200

    task to count clicks, show and reset result substraction and addition a number

---
+++ react/CountClicksAndShowResult.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import './App.css';
+
+
+class App extends React.Component{
+  state={
+    count:0,
+    result: 0
+  }
+
+  handleMathClick = (type, number) => {
+    if(type ==="substraction"){
+      this.setState(prevState => ({
+        count: prevState.count + 1,
+        result: prevState.result - number
+      }))
+    }else{
+      this.setState(prevState => ({
+        count: prevState.count +1,
+        result: prevState.result + number
+      }))} 
+  }
+
+
+  handleResetButton=()=>{
+    this.setState({
+      result: 0
+    })
+
+  }
+
+  render(){
+    return(
+      <>
+        <button onClick={() => this.handleMathClick("substraction",1)}>-1</button>
+        <button onClick = {this.handleResetButton}>Reset</button>
+        <button onClick= {()=> this.handleMathClick("addition", 1)}>+1</button>
+        <h1>Number of clicks: {this.state.count}</h1>
+        <h1>Result: {this.state.result}</h1>
+      </>
+    )
+  }
+}
+   
+
+export default App
